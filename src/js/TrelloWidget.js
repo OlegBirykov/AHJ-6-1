9b6c298d3b43008569e09413a135b2d3a39955d6
OlegBirykov <p_petro75@mail.ru>
2020-10-12 23:10:00 +0300

    Добавлен перенос графики без переноса данных

--- src/js/TrelloWidget.js
+++ src/js/TrelloWidget.js
@@ -1,6 +1,8 @@
 export default class TrelloWidget {
   constructor(container) {
     this.container = container;
+    this.data = [];
+    this.dragEl = null;
   }
 
   static get markup() {
@@ -20,7 +22,7 @@ export default class TrelloWidget {
           <h3>${title}</h3>
           <a href="#" data-id="${this.ctrlId.headerMenu}">&#x22EF;</a>
         </header>
-        <div daia-id="${this.ctrlId.cards}">
+        <div data-id="${this.ctrlId.cards}">
         </div>
         <footer>
           <a href="#" data-id="${this.ctrlId.showForm}" data-visible="yes">+Add another card</a>
@@ -44,7 +46,7 @@ export default class TrelloWidget {
       headerMenu: 'menu',
       cards: 'card-list',
       card: 'card',
-      delete: 'delete-button',
+      deleteCard: 'delete-button',
       showForm: 'add-form-show',
       form: 'add-form',
       newCardText: 'add-form-text',
@@ -75,7 +77,7 @@ export default class TrelloWidget {
   }
 
   static get deleteSelector() {
-    return `[data-id=${this.ctrlId.delete}]`;
+    return `[data-id=${this.ctrlId.deleteCard}]`;
   }
 
   static get showFormSelector() {
@@ -107,52 +109,166 @@ export default class TrelloWidget {
     this.widget = this.container.querySelector(this.constructor.widgetSelector);
 
     this.cols = this.widget.querySelectorAll(this.constructor.columnSelector);
+    this.cards = [];
+    this.showFormButtons = [];
+    this.forms = [];
+    this.textareas = [];
+    this.addCardButtons = [];
+    this.hideFormButtons = [];
 
-    this.cols.forEach((column) => {
-      column.addEventListener('click', (event) => this.onClick(column, event));
+    this.cols.forEach((column, index) => {
+      this.cards.push(column.querySelector(this.constructor.cardsSelector));
+      this.showFormButtons.push(column.querySelector(this.constructor.showFormSelector));
+      this.forms.push(column.querySelector(this.constructor.formSelector));
+      this.textareas.push(column.querySelector(this.constructor.newCardTextSelector));
+      this.addCardButtons.push(column.querySelector(this.constructor.addCardSelector));
+      this.hideFormButtons.push(column.querySelector(this.constructor.hideFormSelector));
+
+      const col = column;
+      col.dataset.index = index;
+      col.addEventListener('click', (event) => this.onClick(column.dataset.index, event));
     });
+
+    this.widget.addEventListener('pointerdown', (event) => this.onDragStart(event));
+    this.widget.addEventListener('pointermove', (event) => this.onDrag(event));
+    this.widget.addEventListener('pointerup', (event) => this.onDragEnd(event));
+
+    this.redraw();
   }
 
-  onClick(column, event) {
+  onClick(colIndex, event) {
     switch (event.target.dataset.id) {
       case this.constructor.ctrlId.showForm:
         event.preventDefault();
-        this.showForm(column);
+        this.showForm(colIndex);
         break;
 
       case this.constructor.ctrlId.hideForm:
         event.preventDefault();
-        this.hideForm(column);
+        this.hideForm(colIndex);
         break;
 
       case this.constructor.ctrlId.addCard:
-        this.addCard(column, event);
+        this.addCard(colIndex, event);
+        break;
+
+      case this.constructor.ctrlId.deleteCard:
+        event.preventDefault();
+        this.deleteCard(colIndex, event.target.dataset.index);
         break;
 
       default:
     }
   }
 
-  showForm(column) {
-    const button = column.querySelector(this.constructor.showFormSelector);
-    button.dataset.visible = 'no';
-    const form = column.querySelector(this.constructor.formSelector);
-    form.dataset.visible = 'yes';
+  onDragStart(event) {
+    if (event.target.dataset.id !== this.constructor.ctrlId.card) {
+      return;
+    }
+    event.preventDefault();
+
+    this.dragEl = event.target.cloneNode(true);
+    this.dragEl.dataset.id = 'card-drag';
+    this.widget.appendChild(this.dragEl);
+
+    const {
+      x, y, width,
+    } = event.target.getBoundingClientRect();
+    this.dragEl.style.left = `${x}px`;
+    this.dragEl.style.top = `${y}px`;
+    this.dragEl.style.width = `${width}px`;
+
+    this.deltaXDrag = x - event.pageX;
+    this.deltaYDrag = y - event.pageY;
   }
 
-  addCard(column, event) {
-    const textarea = column.querySelector(this.constructor.newCardTextSelector);
+  onDrag(event) {
+    if (!this.dragEl) {
+      return;
+    }
+    event.preventDefault();
+    this.dragEl.style.left = `${event.pageX + this.deltaXDrag}px`;
+    this.dragEl.style.top = `${event.pageY + this.deltaYDrag}px`;
+  }
+
+  onDragEnd(event) {
+    if (!this.dragEl) {
+      return;
+    }
+    event.preventDefault();
+    this.dragEl.remove();
+    this.dragEl = null;
+  }
+
+  showForm(colIndex) {
+    this.showFormButtons[colIndex]
+      .dataset.visible = 'no';
+    this.forms[colIndex]
+      .dataset.visible = 'yes';
+  }
+
+  hideForm(colIndex) {
+    this.textareas[colIndex].value = '';
+    this.showFormButtons[colIndex]
+      .dataset.visible = 'yes';
+    this.forms[colIndex]
+      .dataset.visible = 'no';
+  }
+
+  addCard(colIndex, event) {
+    const textarea = this.textareas[colIndex];
     textarea.value = textarea.value.trim();
     if (!textarea.value) return;
 
     event.preventDefault();
-    this.hideForm(column);
+
+    this.data[colIndex].push(textarea.value);
+
+    this.hideForm(colIndex);
+    this.saveData();
+    this.redraw();
   }
 
-  hideForm(column) {
-    const button = column.querySelector(this.constructor.showFormSelector);
-    button.dataset.visible = 'yes';
-    const form = column.querySelector(this.constructor.formSelector);
-    form.dataset.visible = 'no';
+  deleteCard(colIndex, rowIndex) {
+    this.data[colIndex].splice(rowIndex, 1);
+
+    this.saveData();
+    this.redraw();
+  }
+
+  loadData() {
+    try {
+      this.data = JSON.parse(localStorage.getItem('data'));
+    } catch (e) {
+      this.data = [[], [], []];
+      throw new Error('Invalid data');
+    }
+
+    if (!this.data) {
+      this.data = [[], [], []];
+    }
+  }
+
+  saveData() {
+    localStorage.setItem('data', JSON.stringify(this.data));
+  }
+
+  redraw() {
+    this.loadData();
+
+    this.data.forEach((columnData, index) => {
+      let html = '';
+      columnData.forEach((cardData, cardIndex) => {
+        html += `
+          <div data-id="${this.constructor.ctrlId.card}">
+            <p>
+              ${cardData}
+            </p>
+            <a href="#" data-id="${this.constructor.ctrlId.deleteCard}" data-index="${cardIndex}">&#x274C;</a>
+          </div>      
+        `;
+      });
+      this.cards[index].innerHTML = html;
+    });
   }
 }
